29afe5296b2714f9ccbda625fc81843a4c987f99
Braz Magri Junior <est.braz@seap.pr.gov.br>
2025-06-04 16:39:13 -0300

    Ajustes+

--- src/components/ParallaxSection.tsx
+++ src/components/ParallaxSection.tsx
@@ -2,37 +2,37 @@ import { Box } from "@mui/material";
 import type { ReactNode } from "react";
 
 interface ParallaxSectionProps {
-  image: string;
-  height: string;
-  children: ReactNode;
+	image: string;
+	height: string;
+	children: ReactNode;
 }
 
 const ParallaxSection: React.FC<ParallaxSectionProps> = ({
-  image,
-  height,
-  children,
+	image,
+	height,
+	children,
 }) => {
-  return (
-    <Box
-      sx={{
-        minHeight: `${height}`,
-        backgroundImage: `url(${image})`,
-        backgroundAttachment: "fixed",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        display: {xs: "block", md: "flex"},
-        alignItems: "center",
-        justifyContent: "space-around",
-        color: "white",
-        textAlign: "center",
-        boxShadow: "inset 0 0 1em #333",
-        p: 4,
-      }}
-    >
-      {children}
-    </Box>
-  );
+	return (
+		<Box
+			sx={{
+				minHeight: `${height}`,
+				backgroundImage: `url(${image})`,
+				backgroundAttachment: "fixed",
+				backgroundPosition: "center",
+				backgroundRepeat: "no-repeat",
+				backgroundSize: "cover",
+				display: { xs: "block", md: "flex" },
+				alignItems: "center",
+				justifyContent: "space-around",
+				color: "white",
+				textAlign: "center",
+				boxShadow: "inset 0 0 1em #333",
+				p: 4,
+			}}
+		>
+			{children}
+		</Box>
+	);
 };
 
 export default ParallaxSection;
--- src/components/SectionImage.tsx
+++ src/components/SectionImage.tsx
@@ -2,27 +2,27 @@ import { Box } from "@mui/material";
 import type { SxProps } from "@mui/material";
 
 interface SectionImageProps {
-  url: string;
-  size?: number; // opcional
-  sx?: SxProps; // extra: para estilizações adicionais
+	url: string;
+	size?: number; // opcional
+	sx?: SxProps; // extra: para estilizações adicionais
 }
 
 const SectionImage: React.FC<SectionImageProps> = ({ url, size, sx }) => {
-  return (
-    <Box
-      component="img"
-      src={url}
-      alt=""
-      loading="lazy"
-      sx={{
-        // width: "100%",
-        maxWidth: size,
-        // height: "auto",
-        display: "block",
-        ...sx, // permite sobrescrever estilos se quiser
-      }}
-    />
-  );
+	return (
+		<Box
+			component="img"
+			src={url}
+			alt=""
+			loading="lazy"
+			sx={{
+				// width: "100%",
+				maxWidth: size,
+				// height: "auto",
+				display: "block",
+				...sx, // permite sobrescrever estilos se quiser
+			}}
+		/>
+	);
 };
 
 export default SectionImage;
